e7bcd6a6bfa45d10f080e64bd4399848bbcf957e
CrystalDzib <89056187+CrystalDzib@users.noreply.github.com>
2021-09-28 00:44:49 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+    ?>
+<div class="navbar-fixed">
+
+    <nav class="blue">
+        <div class="nav-wrapper container">
+            <a href="#!" class="brand-logo right">Crystal Dzib </a>
+            <ul class="left">
+                <li><a href="?menu=bienvenida">Inicio</a></li>
+                <li><a href="?menu=acercademi">Acerca de mi ...</a></li>
+                <li><a href="?menu=viajes">Viajes</a></li>
+                <li><a href="?menu=escuela">Escuela</a></li>
+                <li><a href="?menu=escuela">Alumno</a></li>
+                <li><a href="?menu=footer">Redes Sociales</a></li>
+                <li><a href="?menu=login">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
